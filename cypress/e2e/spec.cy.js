0a1cccc7d9f915e08e5b9656bb4f5776de213a27
Lucas Eduardo Beninca <lebbeninca@gmail.com>
2023-08-04 07:56:16 -0300

    insertion assert

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -180,6 +180,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
 it('seleciona um arquivo da pasta fixtures', function(){
 cy.get('input[type="file"]')
+.should('not.have.value')
 .selectFile('cypress/fixtures/example.json')
 .then(input => {
   expect(input[0].files[0].name).to.equal('example.json');
